d17ee3a5c0a3a2bfcc72dc0c38d0527cf54d11ab
Ajith G <137430019+Ajith-G-1997@users.noreply.github.com>
2023-07-14 16:59:17 +0530

    Update urls.py
    remove all comments code

--- bookstore/books/urls.py
+++ bookstore/books/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .views import RegisterUser,BookListAPIView, BookDetailAPIView, BookCreateAPIView, BookUpdateAPIView, BookDeleteAPIView
-# from rest_framework.authtoken.views import obtain_auth_token
+
 
 
 urlpatterns = [
     path('register/', RegisterUser.as_view(), name='register'),
-    #path('login/',obtain_auth_token,name="login"),
+    
    
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
