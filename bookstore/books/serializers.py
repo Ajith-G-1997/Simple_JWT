3c89dfc7b2b59ddd6fa56227f96d254758acb6af
Ajith G <137430019+Ajith-G-1997@users.noreply.github.com>
2023-07-14 16:57:58 +0530

    Update serializers.py
    remove all comments code

--- bookstore/books/serializers.py
+++ bookstore/books/serializers.py
@@ -4,38 +4,6 @@ from books.models import User
 from rest_framework_simplejwt.tokens import RefreshToken
 
 
-# class UserSerializer(serializers.ModelSerializer):
-#     token = serializers.SerializerMethodField()
-
-#     def get_token(self, obj):
-#         refresh = RefreshToken.for_user(obj)
-#         return {
-#             'refresh': str(refresh),
-#             'access': str(refresh.access_token),
-#         }
-
-#     class Meta:
-#         model = User
-#         fields = ['username', 'first_name', 'last_name', 'email', 'password','token']
-#         extra_kwargs = {'password': {'write_only': True}}
-
-
-#         def save(self):
-#             reg = User(
-#             username=self.validated_data['username'],
-#             email=self.validated_data['email'],
-#             first_name=self.validated_data['first_name'],
-#             last_name=self.validated_data['last_name'],
-#             )
-#             password = self.validated_data['password']
-#             reg.set_password(password)
-
-
-        
-#             reg.save()
-
-
-#             return reg
 
 
 class UserSerializer(serializers.ModelSerializer):
