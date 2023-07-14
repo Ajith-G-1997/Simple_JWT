db31e978510e099bc3a4363a2c083ce4bfb30d36
Ajith G <137430019+Ajith-G-1997@users.noreply.github.com>
2023-07-14 17:00:02 +0530

    Update urls.py
    remove all comments code

--- bookstore/bookstore/urls.py
+++ bookstore/bookstore/urls.py
@@ -1,14 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-# from rest_framework.routers import DefaultRouter
-# from books.views import BookViewSet
 
-# router = DefaultRouter()
-# router.register('books', BookViewSet)
 
 urlpatterns = [
     
     path('admin/', admin.site.urls),
-    # path('api/', include(router.urls)),
     path('',include('books.urls'))
 ]
